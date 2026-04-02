93ef10b44d4d83ab79954658358f78f54f894261
MAJETI JASWANTH SAI <jaswanthsaimajeti@gmail.com>
2026-04-02 23:20:57 +0530

    Initial project setup with Vite and Tailwind CSS v3

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-3xl font-bold text-blue-500 p-6 text-center">
+        Finance Dashbaord
+    </div>
+  );
+}
+
+export default App
